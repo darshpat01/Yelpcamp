b79f8c5c5914f81f565f41ef17451611bad5f28c
darshpat01 <darshpat01@gmail.com>
2021-07-20 19:59:46 +0530

    added geojson

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -18,14 +18,15 @@ module.exports.createCampground = async (req, res, next) => {
         query: req.body.campground.location,
         limit: 1
     }).send()
-    res.send(geoData.body.features[0].geometry.coordinates);
-    // const campground = new Campground(req.body.campground);
-    // campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    // campground.author = req.user._id; 
-    // await campground.save();
-    // console.log(campground);
-    // req.flash('success', 'Successfully made a new campground'); 
-    // res.redirect(`/campgrounds/${campground._id}`);
+    
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry; 
+    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.author = req.user._id; 
+    await campground.save();
+    console.log(campground);
+    req.flash('success', 'Successfully made a new campground'); 
+    res.redirect(`/campgrounds/${campground._id}`);
 }
 
 module.exports.showCampground = async (req, res) => {
