1a715ecfd5876ed95fde678167dd67e232c15389
darshpat01 <darshpat01@gmail.com>
2021-07-21 01:21:28 +0530

    added popup

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,3 +1,5 @@
+// const campground = require("../../models/campground");
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
@@ -8,4 +10,7 @@ zoom: 8 // starting zoom
 
 new mapboxgl.Marker()
 .setLngLat(campground.geometry.coordinates)
+.setPopup(
+    new mapboxgl.Popup({offset: 25}).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`)
+)
 .addTo(map);
