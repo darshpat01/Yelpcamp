aa56f91b23ac1ea385a588169c615af4f0d886d0
darshpat01 <darshpat01@gmail.com>
2021-07-21 00:43:48 +0530

    connected map to location

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
+center: campground.geometry.coordinates, // starting position [lng, lat]
+zoom: 8 // starting zoom
 });
+
+new mapboxgl.Marker()
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
