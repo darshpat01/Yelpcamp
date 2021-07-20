83c816c0152d242dca79bde32b880abef58b73e7
darshpat01 <darshpat01@gmail.com>
2021-07-20 18:58:46 +0530

    added thumbnails to edit campgrounds

--- models/campground.js
+++ models/campground.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema; 
 
+const ImageSchema = new Schema ({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload','/upload/w_200'); 
+});
+
+
 const CampgroundSchema = new Schema({
     title: String, 
-    images: [{
-        url: String,
-        filename: String
-    }],
+    images: [ImageSchema],
     price: Number,
     description: String, 
     location: String ,
