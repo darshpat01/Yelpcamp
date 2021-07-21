924583311d20b7f2e121261b3f6c04fd2fec7de0
darshpat01 <darshpat01@gmail.com>
2021-07-21 18:26:04 +0530

    more port changes

--- app.js
+++ app.js
@@ -188,6 +188,6 @@ app.use((err, req, res, next) => {
 
 
 const port = process.env.PORT || 3000; 
-app.listen(PORT, () => {
-    console.log(`Serving on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 })
